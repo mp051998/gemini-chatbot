a1fd4f3703722b33bf70eaa4575d430a66fc6396
Monish <mp051998@gmail.com>
2023-12-27 02:02:00 +0530

    Add clear chat history functionality and update chat display

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 
-from config.globals import SPEAKER_TYPES
+from config.globals import SPEAKER_TYPES, initial_prompt
 
 from services.google.generative_ai import GeminiProModelChat
 
@@ -18,21 +18,25 @@ st.set_page_config(
 
 # Initialize a session state to hold the chat history
 if 'chat_history' not in st.session_state:
-  st.session_state.chat_history = []
+  st.session_state.chat_history = [initial_prompt]
+
+def clear_chat_history():
+  st.session_state.chat_history = [initial_prompt]
 
 with st.sidebar:
   st.title('♊💬 Gemini Chatbot')
-  st.write('This chatbot uses Gemini Pro API.')
+  st.write('This chatbot uses Gemini Pro API.')  
+  st.sidebar.button('Clear Chat History', on_click=clear_chat_history, type='primary')
 
 # Get user input and generate response
 prompt = st.chat_input("Ask Gemini Pro a question...", key="user_input")
 if prompt:
-  st.session_state['chat_history'].append((SPEAKER_TYPES.USER, prompt))
+  st.session_state['chat_history'].append({'role': SPEAKER_TYPES.USER, 'content': prompt})
   response_text = chat_conversation.get_gemini_response(prompt, stream=False)
-  st.session_state['chat_history'].append((SPEAKER_TYPES.BOT, response_text))
-    
-# Show the chat
-for speaker, message in st.session_state['chat_history']:
-  with st.chat_message(speaker, avatar="👤" if speaker == SPEAKER_TYPES.USER else "🤖"):
-    st.write(message)
+  st.session_state['chat_history'].append({'role': SPEAKER_TYPES.BOT, 'content': response_text})
+
+# Display or clear chat messages
+for message in st.session_state.chat_history:
+  with st.chat_message(message["role"], avatar="👤" if message['role'] == SPEAKER_TYPES.USER else "🤖"):
+    st.write(message["content"])
 
